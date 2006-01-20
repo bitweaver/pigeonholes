c6486da2f6a238aeeac0266184ac8a098e6c53a4
Max Kremmel <xing@synapse.plus.com>
2006-01-20 17:00:43 +0000

    ensure that all categories are listed

--- assign_non_members.php
+++ assign_non_members.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_pigeonholes/Attic/assign_non_members.php,v 1.3.2.2 2006/01/20 16:46:34 squareing Exp $
+ * $Header: /cvsroot/bitweaver/_bit_pigeonholes/Attic/assign_non_members.php,v 1.3.2.3 2006/01/20 17:00:43 squareing Exp $
  *
  * Copyright ( c ) 2004 bitweaver.org
  * Copyright ( c ) 2003 tikwiki.org
@@ -8,7 +8,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: assign_non_members.php,v 1.3.2.2 2006/01/20 16:46:34 squareing Exp $
+ * $Id: assign_non_members.php,v 1.3.2.3 2006/01/20 17:00:43 squareing Exp $
  * @package pigeonholes
  * @subpackage functions
  */
@@ -82,7 +82,8 @@ $gBitSmarty->assign( 'pigeonRoots', !empty( $pigeonRoots ) ? $pigeonRoots : NULL
 
 $listHash = array(
 	'root_structure_id' => ( !empty( $_REQUEST['root_structure_id'] ) ? $_REQUEST['root_structure_id'] : NULL ),
-	'force_extras' => TRUE
+	'force_extras' => TRUE,
+	'max_records' => -1,
 );
 $pigeonList = $gPigeonholes->getList( $listHash );
 $gBitSmarty->assign( 'pigeonList', !empty( $pigeonList['data'] ) ? $pigeonList['data'] : NULL );
