415d238e6931dbfbec9ecebbcd7e6584e9bb12c1
bitweaver.org <bitweaver@users.sourceforge.net>
2006-02-23 14:58:31 +0000

    assign $gContent by_ref

--- lookup_pigeonholes_inc.php
+++ lookup_pigeonholes_inc.php
@@ -4,7 +4,7 @@
  *
  * @package pigeonholes
  * @subpackage functions
- * @version $Header: /cvsroot/bitweaver/_bit_pigeonholes/lookup_pigeonholes_inc.php,v 1.3 2006/01/24 10:21:23 squareing Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_pigeonholes/lookup_pigeonholes_inc.php,v 1.4 2006/02/23 14:58:31 bitweaver Exp $
  *
  * Copyright ( c ) 2005 bitweaver.org
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -18,5 +18,5 @@ require_once( PIGEONHOLES_PKG_PATH.'/Pigeonholes.php' );
 
 $gContent = new Pigeonholes( ( !empty( $_REQUEST['structure_id'] ) ? $_REQUEST['structure_id'] : NULL ), ( !empty( $_REQUEST['content_id'] ) ? $_REQUEST['content_id'] : NULL ) );
 $gContent->load( TRUE );
-$gBitSmarty->assign( 'gContent', $gContent );
+$gBitSmarty->assign_by_ref( 'gContent', $gContent );
 ?>
