fd827654851c30e5a99659baf78290cdbf8adc3c
Lester Caine <lester@lsces.co.uk>
2006-10-13 12:46:21 +0000

    phpDoc corrections

--- modules/mod_category_menu.php
+++ modules/mod_category_menu.php
@@ -1,4 +1,18 @@
 <?php
+/**
+ * $Header: /cvsroot/bitweaver/_bit_pigeonholes/modules/mod_category_menu.php,v 1.4 2006/10/13 12:46:21 lsces Exp $
+ *
+ * Copyright (c) 2004 bitweaver.org
+ * Copyright (c) 2003 tikwiki.org
+ * Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
+ * All Rights Reserved. See copyright.txt for details and a complete list of authors.
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
+ *
+ * $Id: mod_category_menu.php,v 1.4 2006/10/13 12:46:21 lsces Exp $
+ * @package categories
+ * @subpackage modules
+ */
+
 global $gContent;
 if( !empty( $gContent->mContentId ) ) {
 	if( $gBitUser->hasPermission( 'p_pigeonholes_view' ) ) {
