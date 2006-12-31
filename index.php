ac34a1b13008369560d335882420e3b2658bf4ac
Max Kremmel <xing@synapse.plus.com>
2006-12-31 11:34:30 +0000

    use the displayUrl

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_pigeonholes/index.php,v 1.2 2006/01/24 10:21:23 squareing Exp $
+ * $Header: /cvsroot/bitweaver/_bit_pigeonholes/index.php,v 1.3 2006/12/31 11:34:30 squareing Exp $
  *
  * Copyright ( c ) 2004 bitweaver.org
  * Copyright ( c ) 2003 tikwiki.org
@@ -8,7 +8,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: index.php,v 1.2 2006/01/24 10:21:23 squareing Exp $
+ * $Id: index.php,v 1.3 2006/12/31 11:34:30 squareing Exp $
  * @package pigeonholes
  * @subpackage functions
  */
@@ -19,7 +19,8 @@
 require_once( '../bit_setup_inc.php' );
 include_once( PIGEONHOLES_PKG_PATH.'lookup_pigeonholes_inc.php' );
 if( !empty( $gContent->mStructureId ) ) {
-	header( 'Location: '.PIGEONHOLES_PKG_URL.'view.php?structure_id='.$gContent->mStructureId );
+	header( 'Location: '.$gContent->getDisplayUrl() );
 } else {
 	header( 'Location: '.PIGEONHOLES_PKG_URL.'list.php' );
 }
+die;
